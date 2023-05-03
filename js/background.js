ef09aa18bd13202b50d51b9aafe2c8cb6ecf0e91
three3ape <129493280+three3ape@users.noreply.github.com>
2023-05-03 16:23:52 +0900

    new images

--- js/background.js
+++ js/background.js
@@ -10,13 +10,18 @@ const images = [
   "09.jpg",
   "10.jpg",
   "11.jpg",
+  "12.jpg",
+  "13.jpg",
+  "14.jpg",
+  "15.jpg",
+  "16.jpg",
 ];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 // const background = document.querySelector("#background");
 // const bgImage = document.createElement("img");
 const body = document.querySelector("body");
-body.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.4),rgba(0, 0, 0, 0.4)), url(img/${chosenImage})`;
+body.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)), url(img/${chosenImage})`;
 
 // bgImage.src = `img/${chosenImage}`;
 
